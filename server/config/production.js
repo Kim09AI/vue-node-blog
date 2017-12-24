0eb0518162000ecc418ae0d48b4d3114e5205a7b
Kim09 <1695731158@qq.com>
2017-12-24 14:28:51 +0800

    恢复配置

---
+++ server/config/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongodb: 'mongodb://_ss:snsdsone@ds131137.mlab.com:31137/vue_node_blog',
+    homeUrl: '/',
+    'client_id': '50c1ee417a637be79b3e',
+    'client_secret': 'fa49d95323a7548ff359fb3630c7787323468f26',
+}
